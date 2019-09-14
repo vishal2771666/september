053e88ba8998ac3c1c98a5634cbabbf470b1d376
Ashwani <aswanibtech@gmail.com>
2019-09-14 15:30:15 +0530

    math module tutorial

---
+++ firstmathexp.py
@@ -0,0 +1,13 @@
+#import math
+#print(math.sqrt(36))
+'''
+multiline comment
+'''
+'''import math as m
+print(m.sqrt(36))'''
+
+'''from math import sqrt
+print(sqrt(36))'''
+
+from math import *
+print(sqrt(36))
